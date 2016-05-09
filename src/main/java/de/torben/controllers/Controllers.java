72a129af271780c9874e0dd60e31dbbc1c8c5374
Torben Jessen <derjessen@gmail.com>
2016-05-09 18:00:53 +0200

    aufgeräumt

--- src/main/java/de/torben/controllers/Controllers.java
+++ src/main/java/de/torben/controllers/Controllers.java
@@ -1,12 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package de.torben.controllers;
 
-import org.iban4j.CountryCode;
-import org.iban4j.Iban;
 import org.iban4j.IbanFormatException;
 import org.iban4j.IbanUtil;
 import org.iban4j.InvalidCheckDigitException;
@@ -19,7 +12,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  *
- * @author raGe
+ * @author Torben Jessen
  */
 @Controller
 public class Controllers {
@@ -28,21 +21,21 @@ public class Controllers {
     @ResponseBody
     String home(@PathVariable("iban") String iban) {
         String rofl;
-        try{
+        try {
             IbanUtil.validate(iban);
             rofl = "ya geht";
-        } catch (IbanFormatException | InvalidCheckDigitException | UnsupportedCountryException e){
+        } catch (IbanFormatException | InvalidCheckDigitException | UnsupportedCountryException e) {
             rofl = e.getMessage();
         }
         return rofl;
     }
-    
+
     @RequestMapping("/")
     @ResponseBody
     String index() {
         return "<h1>index</h1>";
     }
-    
+
     @RequestMapping("/deineMudda")
     @ResponseBody
     String mudda() {
